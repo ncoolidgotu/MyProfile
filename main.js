f610338ce1228058638135e430972d182fd3e10e
ncoolidgotu <nathanael.coolidge@ontariotechu.net>
2022-09-28 18:18:56 -0400

    modified:   .DS_Store 	modified:   contact.html 	modified:   full.css 	new file:   github.png 	new file:   homeIcon.png 	modified:   index.html 	new file:   main.js 	new file:   mobile.css 	new file:   phone.css 	new file:   projects.html 	new file:   pyos.png 	new file:   restaurant.png 	modified:   style.css 	new file:   tablet.css

---
+++ main.js
@@ -0,0 +1,8 @@
+function mobileNav() {
+  var x = document.getElementById("mobileLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
