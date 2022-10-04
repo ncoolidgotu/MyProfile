750f22cd658ec3f53fcfb2c47cec8b2b6b4b3e0b
ncoolidgotu <nathanael.coolidge@ontariotechu.net>
2022-10-04 00:28:29 -0400

    modified:   .DS_Store 	modified:   about.html 	deleted:    apple.png 	deleted:    bg.gif 	deleted:    cisco.png 	modified:   contact.html 	modified:   dsl.jpeg 	modified:   fixcomp.png 	modified:   full.css 	deleted:    github.png 	modified:   homeIcon.png 	modified:   index.html 	modified:   intro.mp4 	modified:   intro.ogv 	modified:   main.js 	deleted:    microsoft.webp 	modified:   mobile.css 	new file:   nas.webp 	modified:   nate.jpeg 	modified:   otu.png 	modified:   phone.css 	modified:   projects.html 	modified:   pyos.png 	modified:   restaurant.mp4 	modified:   restaurant.ogv 	deleted:    restaurant.png 	modified:   server.jpg 	modified:   style.css 	modified:   tablet.css 	modified:   tacops.jpg 	new file:   unraid.jpeg 	modified:   virtualbox.png 	modified:   win_95.png

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+//Code below is from W3 Schools
+//https://www.w3schools.com/css/css_navbar.asp
 function mobileNav() {
-  var x = document.getElementById("mobileLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var x = document.getElementById("mobileLinks"); //grab the div class for mobileLinks (navigation for phone/tablet)
+  if (x.style.display === "block") { 
+    x.style.display = "none"; //By default, this class is set to hidden display in CSS
   } else {
-    x.style.display = "block";
-  }
+    x.style.display = "block"; //Unhide the class and display the menu
+}
 }
